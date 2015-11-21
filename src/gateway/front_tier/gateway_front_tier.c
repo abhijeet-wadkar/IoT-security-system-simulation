d8e47d230980ab4e96966e7ab3121c168d6ac119
DevendraDahiphale <devdahiphale@umbc.edu>
2015-11-20 22:48:58 -0500

    Gateway: Modified inference logic

--- src/gateway/front_tier/gateway_front_tier.c
+++ src/gateway/front_tier/gateway_front_tier.c
@@ -270,6 +270,9 @@ void* message_handler(void *context)
 					{
 						strcpy(buffer, "Security Alert - Intruder Detected\n");
 						LOG_INFO(("INFO: Security Alert\n"));
+						gateway->motion_state = -1;
+						gateway->door_state = -1;
+						gateway->key_state = -1;
 					}
 				}
 			}
@@ -282,7 +285,7 @@ void* message_handler(void *context)
 				}
 			}
 		}
-		else if(sensor_status[0] > sensor_status[2])
+		else if(sensor_status[0] > sensor_status[2] && sensor_status[2] == 3)
 		{
 			if(gateway->motion_state == 1)
 			{
@@ -300,6 +303,9 @@ void* message_handler(void *context)
 			{
 				strcpy(buffer, "Security Alert - Someone else in the house\n");
 				LOG_INFO(("INFO: last: Security Alert\n"));
+				gateway->motion_state = -1;
+				gateway->door_state = -1;
+				gateway->key_state = -1;
 			}
 		}
 		if(strlen(buffer))
